a2cabf2d562bba9799c5c69d486d00696fa3a508
Callum Carnegie <ccarnegie98@gmail.com>
2022-11-10 00:11:18 +0000

    Adding basic model for the users/products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	# the model for each product item
+	name = models.CharField("The name of the product", max_length=200)
+	price = models.DecimalField("The price of the product", max_digits=5, decimal_places=2)
+	description = models.TextField("A text description for the item.")
+	height = models.DecimalField("The height of the product.", max_digits=5, decimal_places=2)
+	width = models.DecimalField("The width of the product.", max_digits=5, decimal_places=2)
+	length = models.DecimalField("The length of the product.", max_digits=5, decimal_places=2)
+
+class User(models.Model):
+	# the model for each user
+	username = models.CharField("What the user uses to login", max_length=20)
+	name = models.CharField("What the user chooses to go by", max_length=100)
+	password = ""
+	email = models.EmailField("The user's email for order confirmations.")
+	joined = models.DateField("Date the user joined", auto_now_add=True)
